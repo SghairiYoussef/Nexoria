68112419ab37a653f709aed3956fed710f63ba14
Youssef Sghairi <sghairiyoussef10@gmail.com>
2025-01-31 22:04:28 +0100

    fixed animations

--- src/entities/player.js
+++ src/entities/player.js
@@ -8,10 +8,7 @@ export function makePlayer(k, initialPos) {
             shape: new k.Rect(k.vec2(0, 18), 24, 82),
         }),
         k.anchor("center"),
-        k.body({
-            mass: 100,
-            jumpForce: 320,
-        }),
+        k.body(), // No need to define mass & jumpForce here
         k.opacity(1),
         k.health(3),
         k.scale(0.75),
@@ -20,77 +17,110 @@ export function makePlayer(k, initialPos) {
             speed: PLAYER_SPEED,
             isAttacking: false,
             isShielding: false,
+
             setPosition(x, y) {
                 this.pos.x = x;
                 this.pos.y = y;
             },
+
             setSprite(spriteName, animName) {
                 if (this.curAnim() === animName) return;
                 this.use(k.sprite(spriteName));
                 this.play(animName);
-                console.log("setSprite", this.curAnim())
+                console.log("setSprite", this.curAnim());
             },
+
             setControls() {
                 this.controlHandlers = [];
 
+                // Jump Fix: Check `this.isGrounded()` instead of `this.isGrounded`
                 this.controlHandlers.push(
-                    k.onKeyPress((key) => {
-                        if (key === "space") {
-                            if (true /*TODO*/) {
-                                this.jump(this.jumpForce);
-                                this.setSprite("playerJump", "jump");
-                            }
-                        }
+                    k.onKeyPress("space", () => {
+                        if (this.isGrounded()) {
+                            this.jump(320); // Use actual jump force
+                            this.setSprite("playerJump", "jump");
 
-                        if (key === "w") {
-                            if (!this.isAttacking && !this.isShielding) {
-                                this.isAttacking = true;
-                                this.setSprite("playerAttack", "attack");
-                                console.log("attack", this.curAnim())
+                            // Ensure that jump animation does not reset too soon
+                            k.wait(0.2, () => {
+                                if (!this.isGrounded()) {
+                                    this.setSprite("playerJump", "jump");
+                                }
+                            });
+                        }
+                    })
+                );
+
+                this.controlHandlers.push(
+                    k.onKeyPress("w", () => {
+                        if (!this.isAttacking && !this.isShielding) {
+                            this.isAttacking = true;
+                            this.setSprite("playerAttack", "attack");
+                            console.log("attack", this.curAnim());
+
+                            // Delay before resetting isAttacking
+                            k.wait(0.5, () => {
                                 this.isAttacking = false;
-                            }
+                                if (this.isGrounded()) this.setSprite("playerIdle", "idle");
+                            });
                         }
+                    })
+                );
 
-                        if (key === "x") {
-                            if (!this.isAttacking && !this.isShielding) {
-                                this.isShielding = true;
-                                this.setSprite("playerShield", "shield");
-                            }
+                this.controlHandlers.push(
+                    k.onKeyPress("x", () => {
+                        if (!this.isAttacking && !this.isShielding) {
+                            this.isShielding = true;
+                            this.setSprite("playerShield", "shield");
+
+                            k.wait(0.5, () => {
+                                this.isShielding = false;
+                                if (this.isGrounded()) this.setSprite("playerIdle", "idle");
+                            });
                         }
                     })
                 );
 
                 this.controlHandlers.push(
                     k.onKeyDown((key) => {
-                    if (key === "left") {
-                        this.move(-this.speed, 0);
-                        this.flipX = true;
-                        if (true /*TODO*/&& !this.isAttacking && this.isGrounded) {
-                            this.setSprite("playerWalk", "walk");
+                        if (key === "left") {
+                            this.move(-this.speed, 0);
+                            this.flipX = true;
+                            if (!this.isAttacking && this.isGrounded()) {
+                                this.setSprite("playerWalk", "walk");
+                            }
+                        } else if (key === "right") {
+                            this.move(this.speed, 0);
+                            this.flipX = false;
+                            if (!this.isAttacking && this.isGrounded()) {
+                                this.setSprite("playerWalk", "walk");
+                            }
                         }
-                    } else if (key === "right") {
-                        this.move(this.speed, 0);
-                        this.flipX = false;
-                        if (true /*TODO*/ && !this.isAttacking && this.isGrounded) {
-                            this.setSprite("playerWalk", "walk");
-                        }
-                    }
                     })
                 );
+
                 this.controlHandlers.push(
                     k.onKeyRelease(() => {
-                        if (this.curAnim() === "walk") {
+                        if (this.isGrounded() && this.curAnim() === "walk") {
                             this.setSprite("playerIdle", "idle");
                         }
                     })
-                )
+                );
+
+                // Ensure sprite resets after landing
+                this.controlHandlers.push(
+                    k.onUpdate(() => {
+                        if (this.isGrounded() && this.curAnim() === "jump") {
+                            this.setSprite("playerIdle", "idle");
+                        }
+                    })
+                );
             },
 
-            disablleControls() {
+            disableControls() {
                 for (const handler of this.controlHandlers) {
                     handler.cancel();
                 }
-            }
+            },
         }
     ]);
 }
--- src/kaboomLoader.js
+++ src/kaboomLoader.js
@@ -17,7 +17,7 @@ const loadPlayerSprites = () => {
   const sprites = [
     { name: "playerIdle", path: "./assets/Player/Idle.png", sliceX: 6, anims: { idle: { from: 0, to: 5, loop: true } } },
     { name: "playerRun", path: "./assets/Player/Run.png", sliceX: 8, anims: { run: { from: 0, to: 7, loop: true } } },
-    { name: "playerJump", path: "./assets/Player/Jump.png", sliceX: 12, anims: { jump: { from: 0, to: 11 } } },
+    { name: "playerJump", path: "./assets/Player/Jump.png", sliceX: 12, anims: { jump: { from: 0, to: 11, speed: 12 } } },
     { name: "playerAttack", path: "./assets/Player/Attack_1.png", sliceX: 5, anims: { attack: { from: 0, to: 4 } } },
     { name: "playerDeath", path: "./assets/Player/dead.png", sliceX: 4, anims: { death: { from: 0, to: 3 } } },
     { name: "playerWalk", path: "./assets/Player/Walk.png", sliceX: 8, anims: { walk: { from: 0, to: 7, loop: true } } },
